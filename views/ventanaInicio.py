4064c2ee43df02f413e47615291315b9da0ec340
EmaEchazuPers <ema.echazu2@gmail.com>
2023-08-03 13:12:15 -0300

    Actualizacion vista inicio

--- views/ventanaInicio.py
+++ views/ventanaInicio.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter.font import Font
+import tkintermapview as tkMapa
 
 class VentanaInicio(tk.Frame):
     def __init__(self, master=None, controlador=None):
@@ -18,19 +19,27 @@ class VentanaInicio(tk.Frame):
 
         lugares_font = Font(size=10)
 
-        self.lugares = tk.Label(self, text='LUGARES PARA VISITAR', font=lugares_font,wraplength=150)
-        self.lugares.grid(row=1,column=0, pady=8)
+        self.lugares = tk.Label(self, text='LUGARES PARA VISITAR', font=lugares_font,wraplength=300)
+        self.lugares.grid(row=1,column=0, pady=10, padx=30)
 
         self.lista_lugares = tk.Listbox(self)
-        self.lista_lugares.config(width=50)
+        self.lista_lugares.config(width=30,fg='blue',justify='center')
         #self.lista_lugares.pack()
         self.lista_lugares.grid(row=2,column=0)
         self.actualizar_lista()
 
+        #Define mapa
+
+        self.mapa = tkMapa.TkinterMapView(self, width=300, height=200, corner_radius=0)
+        self.mapa.place(relx=0.5,rely=0.5, anchor=tk.CENTER)
+        self.mapa.set_position(-24.789695,-65.411059)
+        self.mapa.set_zoom(10)
+        self.mapa.grid(row=1,column=1)
+
         #Define botones
 
         self.boton_buscar = tk.Button( self, text='Buscar')
-        self.boton_buscar.grid(row=1,column=1,pady=6,padx=10)
+        self.boton_buscar.grid(row=2,column=0,pady=6,padx=10)
     
     def actualizar_lista(self):
         destinos = self.controlador.getDestinos()
