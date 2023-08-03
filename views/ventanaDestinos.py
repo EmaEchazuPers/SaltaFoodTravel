383463840439d388911f14adadcecb2577e2ccae
EmaEchazuPers <ema.echazu2@gmail.com>
2023-08-03 18:45:24 -0300

    Cambio en vistas

---
+++ views/ventanaDestinos.py
@@ -0,0 +1,68 @@
+import tkinter as tk
+from tkinter.font import Font
+import tkintermapview as tkMapa
+
+class VentanaDestinos(tk.Frame):
+    def __init__(self, master=None, controlador=None, cont_ubicaciones=None):
+
+        super().__init__(master)
+        self.master = master
+        self.controlador = controlador
+        self.cont_ubicaciones = cont_ubicaciones
+
+        #Define titulo de ventana destinos
+
+        lugares_font = Font(size=13)
+
+        self.lugares = tk.Label(self, text='Lista de lugares para visitar', font=lugares_font)
+        #self.lugares.pack(side='top', fill='x',pady=10)
+        self.lugares.grid(row=0,column=0, columnspan=9,pady=5)        
+
+        #Define botones
+
+        self.boton_volver = tk.Button( self, text='Volver',command=self.controlador.volver_inicio)
+        #self.boton_volver.pack(side='bottom',pady=10)
+        self.boton_volver.grid(row=1, column=0, rowspan=1, padx=5)
+        
+        #Define lista de lugares
+
+        lugares_font = Font(size=15)        
+
+        self.lista_lugares = tk.Listbox(self)
+        self.lista_lugares.config(width=30,justify='center')
+        self.lista_lugares.grid(row=1, column=2, columnspan=2, rowspan=3,padx=5)
+        #self.lista_lugares.pack(side='left',fill='both',pady=10)   
+
+        #Define mapa
+
+        self.mapa = tkMapa.TkinterMapView(self, width=200, height=200, corner_radius=0)
+        self.mapa.set_position(-24.789695,-65.411059)
+        self.mapa.set_zoom(13)
+        self.mapa.grid(row=1, column=5, columnspan=3, rowspan=3,padx=5)   
+        #self.mapa.pack(side='right',pady=10)
+        mis_coordenadas = self.colocarUbicaciones()
+        for mar in mis_coordenadas:
+            self.mapa.set_marker(mar[0],mar[1],text='')
+
+        self.actualizar_lista()
+
+    
+    def actualizar_lista(self):
+        destinos = self.controlador.getDestinos()
+        self.lista_lugares.delete(0, tk.END)
+        for destino in destinos:
+            self.lista_lugares.insert(tk.END, destino.nombre)
+    
+    def colocarUbicaciones(self):
+        ubicaciones = self.cont_ubicaciones.getUbicaciones()
+        coordenadas = []
+        for ubi in ubicaciones:
+            coordenadas.append(ubi.coordenadas)
+        return coordenadas
+
+    
+    
+
+        
+    
+
--- views/ventanaInicio.py
+++ views/ventanaInicio.py
@@ -13,36 +13,24 @@ class VentanaInicio(tk.Frame):
         titulo_font = Font(size=20, weight='bold')
 
         self.titulo = tk.Label(self, text='Bienvenido a SALTA FOOD TRAVEL', font=titulo_font)
-        self.titulo.grid(row=0,column=0,pady=5)
+        self.titulo.grid(row=0,columnspan=3, pady=10, sticky='n')
+        
 
-        #Define lista de lugares
+        descripcion_font = Font(size=10)
 
-        lugares_font = Font(size=10)
+        self.descripcion = tk.Label(self,text='Una aplicación diseñana para facilitar la exploración de destinos culinarios.'
+        , font=descripcion_font, wraplength=300)
+        self.descripcion.grid(row=1, columnspan=3, pady=50, sticky='n')
+        
 
-        self.lugares = tk.Label(self, text='LUGARES PARA VISITAR', font=lugares_font,wraplength=300)
-        self.lugares.grid(row=1,column=0, pady=10, padx=30)
+        #Botones
 
-        self.lista_lugares = tk.Listbox(self)
-        self.lista_lugares.config(width=30,fg='blue',justify='center')
-        #self.lista_lugares.pack()
-        self.lista_lugares.grid(row=2,column=0)
-        self.actualizar_lista()
+        self.boton_destinos = tk.Button(self, text='Ir a destinos',command=self.controlador.verDestinos)
+        self.boton_destinos.grid(row=2,column=0,pady=10)
+        
+        self.boton_busqueda = tk.Button(self, text='Buscar destinos')
+        self.boton_busqueda.grid(row=2,column=1,pady=10)
+        
+        self.boton_actividades = tk.Button(self, text='Ver actividades')
+        self.boton_actividades.grid(row=2,column=2,pady=10)
 
-        #Define mapa
-
-        self.mapa = tkMapa.TkinterMapView(self, width=300, height=200, corner_radius=0)
-        self.mapa.place(relx=0.5,rely=0.5, anchor=tk.CENTER)
-        self.mapa.set_position(-24.789695,-65.411059)
-        self.mapa.set_zoom(10)
-        self.mapa.grid(row=1,column=1)
-
-        #Define botones
-
-        self.boton_buscar = tk.Button( self, text='Buscar')
-        self.boton_buscar.grid(row=2,column=0,pady=6,padx=10)
-    
-    def actualizar_lista(self):
-        destinos = self.controlador.getDestinos()
-        self.lista_lugares.delete(0, tk.END)
-        for destino in destinos:
-            self.lista_lugares.insert(tk.END, destino.nombre)
